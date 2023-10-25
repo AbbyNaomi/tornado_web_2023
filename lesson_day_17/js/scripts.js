b6f000beaa787528f04e0fb3e146ab8fd4ffb1c6
Tornado-13 <Tornado-13@Teachers-iMac.local>
2023-10-25 15:12:08 +0800

    add: lesson_17

---
+++ lesson_day_17/js/scripts.js
@@ -0,0 +1,26 @@
+console.log('Lesson Day 17 - JS Conditionals');
+
+
+var myName='Yesui';
+var myAge=16;
+var myJob='Student';
+
+
+let unen=true;
+let hudal=false;
+
+if(hudal){
+    console.log('You are telling me the truth');
+}else{
+    console.log('You are telling me the lie');
+}
+
+let age=68;
+
+if(age>13 && age<19){
+    console.log('You are a teenager!');
+} else if (age>19 && age<65){
+    console.log('You are an adult!');
+}else{
+    console.log('You are senior!');
+}
